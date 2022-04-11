388fa28f5bcbcfefee88c612cfd885a52b7d3af0
GipsyDangerM3 <mikael_dev@hotmail.com>
2022-04-11 14:48:17 -0300

    Defining connectionString

--- CatalogAPI2/Context/AppDbContext.cs
+++ CatalogAPI2/Context/AppDbContext.cs
@@ -10,5 +10,23 @@ namespace CatalogAPI2.Context
 
         public DbSet<Product>? Products { get; set; }
         public DbSet<Category>? Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            //Category
+            mb.Entity<Category>().HasKey(c => c.Id);
+            mb.Entity<Category>().Property(c => c.Name).HasMaxLength(100).IsRequired();
+            mb.Entity<Category>().Property(c => c.Description).HasMaxLength(300).IsRequired();
+
+            //Product
+            mb.Entity<Product>().HasKey(c => c.Id);
+            mb.Entity<Product>().Property(c => c.Name).HasMaxLength(100).IsRequired();
+            mb.Entity<Product>().Property(c => c.Description).HasMaxLength(300);
+            mb.Entity<Product>().Property(c => c.Image).HasMaxLength(100);
+            mb.Entity<Product>().Property(c => c.Price).HasPrecision(14, 2);
+
+            //Relationship
+            mb.Entity<Product>().HasOne<Category>(c => c.Category).WithMany(c => c.Products).HasForeignKey(c => c.Id);
+        }
     }
 }
--- CatalogAPI2/Program.cs
+++ CatalogAPI2/Program.cs
@@ -1,7 +1,14 @@
+using CatalogAPI2.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
